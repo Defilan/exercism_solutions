ab56b0efc8ec44c610291b11975bdb9f4ad75e1f
Christopher P. Maher <chris@mahercode.io>
2018-12-17 00:16:57 -0800

    Added Proverb

--- go/proverb/proverb.go
+++ go/proverb/proverb.go
@@ -1,15 +1,24 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package proverb should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package proverb
+
+import "fmt"
 
 // Proverb should have a comment documenting it.
 func Proverb(rhyme []string) []string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return []string{}
+
+	if rhyme == nil {
+		return []string{}
+	}
+
+	var builder []string
+	count := len(rhyme)
+	for i := range rhyme {
+		var s string
+		if i != count-1 {
+			s = fmt.Sprintf("For want of a %s the %s was lost.", rhyme[i], rhyme[i+1])
+		} else {
+			s = fmt.Sprintf("And all for the want of a %s.", rhyme[0])
+		}
+		builder = append(builder, s)
+	}
+	return builder
 }
