621cdda03c64a78d8f575a634582a1226f4bc3dd
Christopher P. Maher <chris@mahercode.io>
2020-04-25 11:03:04 -0700

    Updated Raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,27 +1,23 @@
 package raindrops
 
 import (
-	"math"
 	"strconv"
-	"strings"
 )
+
 // Convert is the main function of Raindrops, returning the requested information regarding factors.
 func Convert(num int) string {
-	returner := Newreturn(float64(num))
+	returner := ""
+	if num%3 == 0 {
+		returner += "Pling"
+	}
+	if num%5 == 0 {
+		returner += "Plang"
+	}
+	if num%7 == 0 {
+		returner += "Plong"
+	}
 	if returner == "" {
-		return strconv.Itoa(num)
+		returner += strconv.Itoa(num)
 	}
 	return returner
-}
-// Newreturn processes the incoming number and determines if it is a factor.
-func Newreturn(num float64) string{
-	var forerunner strings.Builder
-	numList := []float64{3,5,7}
-	wordList := []string{"Pling","Plang","Plong"}
-	for i, factor := range numList{
-		if num/factor == math.Trunc(num/factor) {
-			forerunner.WriteString(wordList[i])
-		}
-	}
-	return forerunner.String()
 }
