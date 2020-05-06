23c30313f280d64df82178988e06eabdece89a94
Christopher P. Maher <chris@mahercode.io>
2020-05-06 09:13:04 -0700

    Updated Scrabble

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -2,27 +2,23 @@ package scrabble
 
 import "strings"
 
+var points = map[rune]int{
+	'a': 1, 'e': 1, 'i': 1, 'o': 1, 'u': 1, 'l': 1, 'n': 1, 'r': 1, 's': 1, 't': 1,
+	'd': 2, 'g': 2,
+	'b': 3, 'c': 3, 'm': 3, 'p': 3,
+	'f': 4, 'h': 4, 'v': 4, 'w': 4, 'y': 4,
+	'k': 5,
+	'j': 8, 'x': 8,
+	'q': 10, 'z': 10,
+}
+
 //Score will calculate the Scrabble score of an incoming string and return its score as an integer
 func Score(letters string) int {
-	points := map[string]int{
-		"aeioulnrst": 1,
-		"dg":         2,
-		"bcmp":       3,
-		"fhvwy":      4,
-		"k":          5,
-		"jx":         8,
-		"qz":         10,
-	}
 	returner := 0
-	for i := 0; i < len(letters); i++ {
-		for letter, value := range points {
-			for y := 0; y < len(letter); y++ {
-				scrabblechar := string(letters[i])
-				if strings.ToLower(scrabblechar) == string(letter[y]) {
-					returner += value
-				}
-			}
-		}
+	scrabble := strings.ToLower(letters)
+
+	for _, letter := range scrabble {
+		returner += points[letter]
 	}
 	return returner
 }
